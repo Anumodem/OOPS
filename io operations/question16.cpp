307b5bcd3289c215255359ed655d7ebacb9d5ef6
Anumodem <110585951+Anumodem@users.noreply.github.com>
2022-12-26 11:18:54 +0530

    cpp

---
+++ io operations/question16.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+ float s1,s2,s3,s,area;
+ cin>>s1>>s2>>s3;
+ s=(s1+s2+s3)/2;
+ area=sqrt(s*(s-s1)*(s-s2)*(s-s3));
+ cout<<fixed<<setprecision(2)<<area<<endl;
+	return 0;
+}
