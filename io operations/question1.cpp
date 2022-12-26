0348930626de763bd6fffa40df19c73e2bf1a6d5
Anumodem <110585951+Anumodem@users.noreply.github.com>
2022-12-26 11:05:49 +0530

    c++

---
+++ io operations/question1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int weightinearth;
+    float weightinmoon;
+    cin>>weightinearth;
+    weightinmoon=weightinearth*0.166;
+    cout<<weightinmoon;
+
+	return 0;
+}
