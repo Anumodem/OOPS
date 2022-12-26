3d0dc23b80ad3b66752b9331d86d9414d1184777
Anumodem <110585951+Anumodem@users.noreply.github.com>
2022-12-26 11:18:29 +0530

    cpp

---
+++ io operations/question15.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int L,D;
+    cin>>L>>D;
+    int distance= D/0.5;
+    int songs= distance/L+1;
+if(songs!=distance)
+  cout<<songs;
+  else
+  cout<<songs;
+	return 0;
+}
