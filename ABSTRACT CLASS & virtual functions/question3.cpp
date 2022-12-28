fb7ae0280520b0c1ef3b8eb446baea36b588a953
Anumodem <110585951+Anumodem@users.noreply.github.com>
2022-12-28 23:40:48 +0530

    cpp

---
+++ ABSTRACT CLASS & virtual functions/question3.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Cooking
+{
+    public:virtual void recipe()=0;
+};
+class FruitsRatio:public Cooking
+{
+    void recipe(){
+       FruitsRatio obj;obj.recipe();}
+};
+int main()
+{
+
+     int a,b,c; cin>>a>>b>>c;
+     b/=2; c/=4;
+     a=min(a,min(b,c));
+     cout<<7*a;
+}
